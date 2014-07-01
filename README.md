e40aeff864469075d81298f19f87953629520419
GitMyCode <marbouchl@gmail.com>
2014-07-02 00:08:12 +0100

    ajout du map avec comme key les groups

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 algo-defi-cgi
 =============
+
+
+Ce code est hautement inspirer du code donnée par CGI
+---
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by MB on 7/1/14.
@@ -13,19 +12,32 @@ public class Main{
 
 
     public static void main(String[]args){
-        System.out.println("allo");
 
         String lvl1_client = "level1/client-trades.txt";
         String lvl1_tp = "level1/TP-trades.txt";
 
         List<TradeNode> tradesClient = read_and_put_in_list(lvl1_client);
-        List<TradeNode> tradesTP = read_and_put_in_list(lvl1_tp);
+        Map<String,List<TradeNode>> mapTradesClient = map_list_with_group_key(tradesClient);
 
+        List<TradeNode> tradesTP = read_and_put_in_list(lvl1_tp);
+        Map<String,List<TradeNode>> mapTradesTP = map_list_with_group_key(tradesTP);
 
         System.out.println("Les transaction clients");
-        for (int i=0; i < tradesClient.size(); i++){
-            System.out.println(tradesClient.get(i));
+
+        Iterator<String> it = mapTradesClient.keySet().iterator();
+        while (it.hasNext()){
+            String group = it.next();
+            System.out.println("Le group: " + group);
+
+            List<TradeNode> listClient = mapTradesClient.get(group);
+            for (int i=0; i < listClient.size(); i++){
+                System.out.println(listClient.get(i));
+            }
+            System.out.println("\n");
+
         }
+
+
     }
 
 
@@ -49,6 +61,29 @@ public class Main{
         }
 
         return  tradeNodeList;
+    }
+
+
+    /*
+    *
+    * */
+    static private Map<String,List<TradeNode>> map_list_with_group_key(List<TradeNode> tradeList ){
+        Map<String, List<TradeNode>> map = new HashMap<String, List<TradeNode>>();
+
+        for(int i =0; i< tradeList.size() ; i ++){
+            TradeNode theTrade = tradeList.get(i);
+
+            List<TradeNode> listTrades = map.get(theTrade.getGroup());
+            if (listTrades == null){
+                listTrades = new ArrayList<TradeNode>();
+            }
+            listTrades.add(theTrade);
+
+            map.put(theTrade.getGroup(),listTrades);
+
+        }
+
+        return map;
     }
 }
 
